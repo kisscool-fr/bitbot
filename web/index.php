c727b5fdab4032f72456aa27082628d5cb4711bb
KisSCoOl <kisscool@kisscool.net>
2016-08-08 16:43:44 +0200

    Add Policy Privacy Rules for Facebook

--- web/index.php
+++ web/index.php
@@ -30,5 +30,9 @@ $app->post('/facebook', 'Bitbot\\Network\\Facebook::main');
 
 $app->post('/telegram', 'Bitbot\\Network\\Telegram::main');
 
+$app->get('/privacy', function () {
+    return new Response('BitBot Privacy Policy<br/><br/>It\'s just an app for fun & laugh, so we will never collect any data about you.<br/>The message you send to us are never read or analyze because the answer we send to you is completly random.<br/>The only info we have for few milliseconds is a unique user_id send by the platform needed to answer you back.<br/>After the answer is sent, we do not have the info anymore.<br/>Have fun !<br/>-----<br/>The BitBot', 200);
+});
+
 // Run !
 $app->run();
